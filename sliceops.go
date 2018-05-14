be1a74d303779bb61eac57174d750ef0c07ba4af
ZzZzzZzzz- <378414723@qq.com>
2018-05-14 23:52:00 +0800

    s

--- sliceops.go
+++ sliceops.go
@@ -4,10 +4,11 @@ import "fmt"
 
 func printSlice(s []int)  {
 	// 每当cap 装不下时.就自身长度 * 2
-	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	fmt.Printf("%v, len=%d, cap=%d\n", s, len(s), cap(s))
 }
 
 func main()  {
+	fmt.Println("Creating slice")
 	var s []int	// 定义一个s的0值 为 nil 的 slice
 
 	for i := 0; i < 25; i++ {
@@ -15,4 +16,33 @@ func main()  {
 		s = append(s, 2 * i + 1)
 	}
 	fmt.Println(s)
+
+	s1 := []int {2, 4, 6, 8}
+	printSlice(s1)
+
+	s2 := make([]int, 8, 8)
+	s3 := make([]int,10, 32)
+	printSlice(s2)
+	printSlice(s3)
+
+	fmt.Println("copying slice")
+	copy(s2, s1)
+	println(s2)
+
+	fmt.Println("deleteing elements from slice")
+	// 可变参数 加上 ...
+	s2 = append(s2[:3], s2[4:]...)
+	printSlice(s2)
+
+	fmt.Println("Popping from front")
+	front := s2[0]
+	s2 = s2[1:]
+	fmt.Println(front)
+	printSlice(s2)
+
+	fmt.Println("Popping from back")
+	tail := s2[len(s2)-1]
+	s2 = s2[:len(s2)-1]
+	fmt.Println(tail)
+	printSlice(s2)
 }
