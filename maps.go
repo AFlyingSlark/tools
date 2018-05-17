308750409a125448e413da75b218ecf95e032d56
ZzZzzZzzz- <378414723@qq.com>
2018-05-17 23:49:10 +0800

    s

--- maps.go
+++ maps.go
@@ -15,10 +15,30 @@ func main()  {
 
 	fmt.Println(m, m2, m3)
 
-	fmt.Println("traversing map")
+	fmt.Println("----traversing map----")
 
 	// 这个 map 是无序的. 从打印的值 可以看出
 	for k, v := range m {
 		fmt.Println(k, v)
 	}
+
+	fmt.Println("----Getting values------")
+	courseName, ok := m["course"]
+	fmt.Println(courseName, ok)
+	couName, ok := m["cou"]
+	fmt.Println(couName, ok) // 依然可以打印出来. 不过是 empty
+
+	if couName, ok := m["cou"]; ok {
+		fmt.Println(couName)
+	} else {
+		fmt.Println("key does not exist")
+	}
+
+	fmt.Println("--delete values------")
+	name, ok := m["name"]
+	fmt.Println(name, ok)
+
+	delete(m, "name")
+	name, ok = m["name"] //  需要重新赋值下.  要不然就是上面的值
+	fmt.Println(name, ok)
 }
