ca0e2ee40a870b4ed747e43b2d73d2495653f7a6
leo007 <leo007@leo.cc>
2025-02-08 16:23:44 +0800

    优化模版渲染

--- generate/main.go
+++ generate/main.go
@@ -4,66 +4,95 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"strings"
 	"text/template"
+)
+
+var (
+	// 配置和模板文件路径
+	configFile = "config.json"
 )
 
 // 定义配置结构体，字段名必须与 JSON 匹配
 type Config struct {
-	Title   string `json:"title"`
-	Name    string `json:"name"`
-	Age     int    `json:"age"`
-	Company string `json:"company"`
+	Conf      []confData `json:"conf"`
+	Templates string     `json:"templates"`
 }
 
-// 读取 JSON 配置文件
-func loadConfig(filename string) (Config, error) {
-	var config Config
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return config, err
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&config)
-	return config, err
+// 渲染文件名及渲染信息
+type confData struct {
+	FileName string  `json:"fileName"`
+	TplData  tplData `json:"tplData"`
 }
 
+// 模版中需要渲染的字段
+type tplData struct {
+	StructName string `json:"structName"`
+}
+
+// make local
+// chmod +x binaryMain
 func main() {
-	// 配置和模板文件路径
-	configFile := "config.json"
-	templateFile := "http.tpl"
-	outputFile := "http.txt"
-
 	// 1. 读取 JSON 配置
-	config, err := loadConfig(configFile)
+	config, err := loadConfig()
 	if err != nil {
 		log.Fatalf("读取配置文件失败: %v", err)
 		return
 	}
 
-	// 2. 读取模板文件
-	tmpl, err := template.ParseFiles(templateFile)
+	// 2. 使用 ParseGlob 解析整个目录的模板文件
+	tmpl, err := template.ParseGlob(config.Templates)
 	if err != nil {
 		log.Fatalf("读取模板文件失败: %v", err)
 		return
 	}
 
-	// 3. 创建输出文件
-	outFile, err := os.Create(outputFile)
+	// 3. 渲染模板并写入文件
+	for _, value := range config.Conf {
+		outputToFile(tmpl, value)
+	}
+}
+
+// 渲染输出文件
+func outputToFile(tmpl *template.Template, value confData) {
+	var (
+		// 构建文件名
+		tplName    = strings.Join([]string{value.FileName, `tpl`}, `.`)
+		targetFile = strings.Join([]string{value.FileName, `go`}, `.`)
+	)
+
+	outFile, err := os.Create(targetFile)
 	if err != nil {
 		log.Fatalf("创建输出文件失败: %v", err)
+
 		return
 	}
-	defer outFile.Close()
 
-	// 4. 渲染模板并写入文件
-	err = tmpl.Execute(outFile, config)
-	if err != nil {
+	defer func() {
+		_ = outFile.Close()
+	}()
+
+	if err = tmpl.ExecuteTemplate(outFile, tplName, value.TplData); err != nil {
 		log.Fatalf("模板渲染失败: %v", err)
 		return
 	}
 
-	log.Println("模板处理完成，结果已写入", outputFile)
+	log.Println(targetFile, "渲染处理完成,结果已写入")
+}
+
+// 读取 JSON 配置文件
+func loadConfig() (Config, error) {
+	var config Config
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		return config, err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+
+	return config, err
 }
---
+++ generate/models.go
@@ -0,0 +1,7 @@
+package main
+
+/*
+
+标题: modelsA
+
+*/
